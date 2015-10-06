c0f9dc831c5f044ef5dc90b16ba0d1e2153b85d3
lalmas <alphalalmas@yahoo.fr>
2015-10-06 12:16:22 +0200

    Instances creation and configuration with cloud-init

---
+++ playbooks/courtyard-cloud-init-script.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Install git package in instance
+yum -y install git
+
+#Install pip package
+yum -y install python-pip
+
+#Install awcli
+pip install awscli
+
+# Clone drop github repository in /tmp/ansible/
+mkdir /tmp/ansible/ && cd /tmp/ansible/ && git clone https://github.com/Lalmas/drop
+
+# Upload ldap certificates
+aws s3 cp s3://deployutils/identities/dbs.internal/etc/pki/tls/certs/dbs.internal.crt /etc/pki/tls/certs/dbs.internal.crt
+
+# Run dservices scripts
+sudo chmod -R 755 /tmp/ansible/drop/
+sudo /tmp/ansible/drop/src/dservices.py /tmp/ansible/drop/share/tero/worker.xml
