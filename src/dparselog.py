357ad9a9f2eb25bcec8cf37c755bddbfb72cc5fe
Adrian Smith <smith.adriane@gmail.com>
2016-09-16 13:12:17 -0700

    Cleaning up parsing code. Also create events for parse errors.

--- src/dparselog.py
+++ src/dparselog.py
@@ -68,7 +68,7 @@ def generate_regex(format_string, var_regex, regexps):
 class NginxLogParser(object):
 
     def __init__(self):
-        format_string = '$remote_addr - $remote_user [$time_local] "$request" $status $body_bytes_sent "$http_referer" "$http_user_agent" "$http_x_forwarded_for"'
+        format_string = '$remote_addr - $remote_user [$time_local] "$request" $status $body_bytes_sent "$http_referer" "$http_user_agent" "$http_x_forwarded_for"\n'
 
         var_regex = r'\$[a-z_]+'
         ip_num_regex  =  r'[0-9]{1,3}'
@@ -116,7 +116,7 @@ class NginxLogParser(object):
 class GunicornLogParser(object):
 
     def __init__(self):
-        format_string = '''%({X-Forwarded-For}i)s %(l)s %(u)s %(t)s "%(r)s" %(s)s %(b)s "%(f)s" "%(a)s"'''
+        format_string = '''%({X-Forwarded-For}i)s %(l)s %(u)s %(t)s "%(r)s" %(s)s %(b)s "%(f)s" "%(a)s"\n'''
 
         var_regex = r'%\([^)]+\)s'
         ip_num_regex  =  r'[0-9]{1,3}'
@@ -174,123 +174,23 @@ class GunicornLogParser(object):
         return parsed
 
 
-
-def parse_format(to_parse, format_string, var_regex, regexps):
-
-    # format_vars  =  re.findall(var_regex, format_string)
-
-    # var_matches  =  list(re.finditer(var_regex, format_string))
-
-    # var_match_positions  =  [(match.start(),match.end()) for match in var_matches]
-
-    # non_var_indexes  =  [0] + list(itertools.chain(*var_match_positions)) + [len(format_string)]
-
-    # grouped_non_var_indexes = [ (non_var_indexes[i*2],non_var_indexes[i*2+1]) for i in range(len(non_var_indexes)/2)]
-
-    # non_var_strings = [format_string[start:end] for start,end in grouped_non_var_indexes]
-    # escaped_non_var_strings = [re.escape(s) for s in non_var_strings]
-
-    # named_regexps = ['(' + regexps[s] + ')' for i,s in enumerate(format_vars)]
-    # full_regex_pieces = list(itertools.chain(*itertools.izip_longest(escaped_non_var_strings, named_regexps,fillvalue='')))
-
-    # full_regex  =  ''.join(full_regex_pieces[:])
-
-    regex = generate_regex(format_string, var_regex, regexps)
-
-    match  =  regex.search(full_regex,to_parse)
-    if match:
-        return dict(zip(format_vars, match.groups()))
-    else:
-        return None
-
-
-def parse_gunicorn_log(nginx_log):
-    format_string = '''%({X-Forwarded-For}i)s %(l)s %(u)s %(t)s "%(r)s" %(s)s %(b)s "%(f)s" "%(a)s"'''
-
-    var_regex = r'%\([^)]+\)s'
-    ip_num_regex  =  r'[0-9]{1,3}'
-    regexps = {
-        '%({X-Forwarded-For}i)s': ip_num_regex,
-        '%(l)s': r'-',
-        '%(u)s': r'-',
-        '%(t)s': r'\[[^\]]+]',
-        '%(r)s': r'[A-Z]+ .* HTTP/1.[01]',
-        '%(s)s': r'[0-9]{3}',
-        '%(b)s': r'-|[0-9]+',
-        '%(f)s': r'[^"]+',
-        '%(a)s': r'[^"]+',
-    }
-    parsed = parse_format(nginx_log, format_string, var_regex, regexps)
-    if parsed is None:
-        return None
-
-    better_names = {
-        '%({X-Forwarded-For}i)s': 'http_x_forwarded_for',
-        '%(l)s': 'dash',
-        '%(u)s': 'username',
-        '%(t)s': 'time_local',
-        '%(r)s': 'request',
-        '%(s)s': 'status',
-        '%(b)s': 'body_bytes_sent',
-        '%(f)s': 'http_referer',
-        '%(a)s': 'http_user_agent',
-    }
-    parsed = { better_names[k] : v for k,v in parsed.items()}
-    
-    request_regex = r'(?P<http_method>[A-Z]+) (?P<http_path>.*) HTTP/1.[01]'
-    request_match = re.match(request_regex, parsed['request'])
-
-    if request_match:
-        parsed.update(request_match.groupdict())
-    
-    field_types = {
-        'status' : int,
-        'body_bytes_sent': convert_bytes_sent,
-        'time_local': lambda s: parse_date(s[1:-1]),
-    }
-    for k, convert in field_types.items():
-        parsed[k] = convert(parsed[k])
-
-    return parsed
-
-
-def parse_nginx_log(nginx_log):
-    parsed = parse_format(nginx_log, format_string, var_regex, regexps)
-    if parsed is None:
-        return None
-
-    parsed = { k[1:]: v for k,v in parsed.items() }
-
-    field_types = {
-        'status' : int,
-        'body_bytes_sent': int,
-        'time_local': parse_date,
-    }
-    for k, convert in field_types.items():
-        parsed[k] = convert(parsed[k])
-
-    request_regex = r'(?P<http_method>[A-Z]+) (?P<http_path>.*) HTTP/1.[01]'
-    request_match = re.match(request_regex, parsed['request'])
-    if request_match:
-        parsed.update(request_match.groupdict())
-
-    return parsed
-
-
-# with open('/Users/adrian/Downloads/djaoapp.com-access.log-062d8892-20160821') as f:
-#     for line in f:
-#         print parse_nginx_log(line)
-
 gunicorn_test_string = '''108.252.136.229 - - [09/Aug/2016:10:15:32 -0700] "GET / HTTP/1.0" 500 3840 "-" "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.103 Safari/537.36"'''
 nginx_test_string = '''183.129.160.229 - - [20/Aug/2016:03:34:59 +0000] "GET / HTTP/1.1" 444 0 "-" "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.11; rv:47.0) Gecko/20100101 Firefox/47.0" "-"'''
 
-# parse_nginx_log(nginx_test_string)
-
-
-# completed = set()
-
-# with open('state.json','w') as f:
-#     json.dump( list(completed), f)
+def error_event(fname, key, reason, extra=None):
+    body = {
+        'reason': reason,
+        'log-filename': fname,
+        'log-key': key,
+        'dt': datetime.now()
+    }
+    if extra:
+        body.update(extra)
+    return {
+        '_index': 'parse-errors',
+        '_type': 'parse-error',
+        '_source': body,
+    }
 
 def generate_events(root, key):
 
@@ -299,6 +199,7 @@ def generate_events(root, key):
     if not match:
         print 'not a log file? %s' % fname
 
+        yield error_event(fname, key, 'log filename didnt match regexp')
         return
 
     full_path = os.path.join( root, key)
@@ -317,8 +218,15 @@ def generate_events(root, key):
     index = 'logs-%s' % (match.group('log_date'))
     doc_type = 'log'
 
-    nginx_parser = NginxLogParser()
-    gunicorn_parser = GunicornLogParser()
+    if log_folder == 'nginx':
+        parser = NginxLogParser()
+    elif log_folder == 'gunicorn':
+        parser = GunicornLogParser()
+    else:
+        print 'unknown log folder!', log_folder
+        yield error_event(fname, key, 'could not find parser for log folder',
+                          {'log-folder': log_folder})
+        return
 
     error_count = 0
     ok_count = 0
@@ -326,25 +234,25 @@ def generate_events(root, key):
         for i, line in enumerate(f):
             total_count = ok_count + error_count
             if total_count > 100 and (float(error_count)/total_count) > 0.8:
-                print 'too many errors. bailing'
+                print 'too many errors. bailing', full_path
+                yield error_event(fname, key, 'bailing because of too many errors.' )
                 return
 
-            # remove endline
-            line = line[:-1]
             try:
-                if log_folder == 'nginx':
-                    event = nginx_parser.parse(line)
-                elif log_folder == 'gunicorn':
-                    event = gunicorn_parser.parse(line)
-                else:
-                    print 'unknown log folder!', log_folder
-                    continue
+                event = parser.parse(line)
             except Exception, e:
                 print e, line
+                yield error_event(fname, key, 'could not parse log line',
+                                  {'line': line,
+                                   'exception_message': e.message,
+                                   'exception_type': type(e).__name__})
+
                 continue
 
             if event is None:
                 print 'parse error', log_folder, repr(line)
+                yield error_event(fname, key, 'could not parse log line',
+                                  {'line': line})
                 error_count += 1
                 continue
             else:
@@ -368,52 +276,22 @@ def generate_events(root, key):
                 '_source': event
             }
 
+def sanitize_filename(fname):
+    fname  = fname.replace(os.path.sep, '_')
+    fname = re.sub(r'[^a-zA-Z_\-.0-9]', '', fname)
+    fname = re.sub(r'^[^a-zA-Z0-9]+', '', fname)
+    if fname.startswith('.'):
+        fname = fname[1:]
 
-
-# def main(root):
-#     es = Elasticsearch([{'host': 'localhost', 'port': 1234}])
-#     # index = 'logs-test'
-#     doc_type = "log"
-
-#     nginx_parser = NginxLogParser()
-#     gunicorn_parser = GunicornLogParser()
-
-#     paths = os.walk(root)
-#     for dirpath, dirnames, filenames in os.walk(root):
-#         for fname in filenames:
-#             event = generate_event(root, fname)
-#             completed.add(fname)
-
-
-#             if not es.indices.exists(index):
-#                 # es.indices.delete(index)
-#                 es.indices.create(index=index,body={
-#                     "mappings": {
-#                         "log": {
-#                             "properties": {
-#                                 "ip_num": {
-#                                     "type": "ip"
-#                                 }
-#                             }
-#                         }
-#                     }
-#                 })
-
-#             if batch:
-#                 print 'sending batch', len(batch)
-#                 elasticsearch.helpers.bulk(es, batch)
-
-#             completed.add(key)
-            
+    return fname
 
 
 if __name__ == '__main__':
     import sys
-
     root = sys.argv[1]
     key = sys.argv[2]
-    import hashlib
-    outname = 'tmp/%s.gz' % hashlib.sha1(key).hexdigest()
+
+    outname = 'tmp/%s' % sanitize_filename(key)
     if os.path.exists(outname):
         print 'already done'
         sys.exit(0)
@@ -431,7 +309,6 @@ if __name__ == '__main__':
         if os.path.exists(outname):
             os.remove(outname)
         raise e
-    # main(root)
 
 
 
