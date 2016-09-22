1cd7c67e412c741c28566cccffce659940fb1552
Adrian Smith <smith.adriane@gmail.com>
2016-09-22 10:39:35 -0700

    Use index templates for elasticsearch. turn on gzip compression for elaticsearch uploads.

--- src/duploades.py
+++ src/duploades.py
@@ -4,6 +4,7 @@ from elasticsearch import Elasticsearch
 import elasticsearch.helpers
 import json
 import gzip
+import urllib3
 
 def events(fname):
     with gzip.open(fname) as f:
@@ -12,9 +13,107 @@ def events(fname):
     
 completed = set()
 def run():
-    root = '/var/tmp/djaodjin-logs/tmp2'
+    root = '/var/tmp/djaodjin-logs/tmp'
     fs = os.listdir(root)
     es = Elasticsearch([{'host': 'localhost', 'port': 9200}])
+
+    # make sure an index template exists so that new indexes that are
+    # created automatically have the correct type mappings.
+    # http://www.elastic.co/guide/en/elasticsearch/reference/current/indices-templates.html
+    es.indices.put_template(name='logs_template',
+                            body={
+                                "template": "logs-*",
+                                "mappings": {
+                                    "log": {
+                                        "properties": {
+                                            "ip_num": {
+                                                "type": "ip"
+                                            },
+                                            "time_local": {
+                                                "type": "date",
+                                            },
+                                            "s3_key": {
+                                                "type": "string",
+                                                "index": "not_analyzed",
+                                            },
+                                            "host": {
+                                                "type": "string",
+                                                "index": "not_analyzed",
+                                            },
+                                            "request": {
+                                                "type": "string",
+                                                "index": "not_analyzed",
+                                            },
+                                            "http_referer": {
+                                                "type": "string",
+                                                "index": "not_analyzed",
+                                            },
+                                            "http_path": {
+                                                "type": "string",
+                                                "index": "not_analyzed",
+                                            },
+                                            "http_request": {
+                                                "type": "string",
+                                                "index": "not_analyzed",
+                                            },
+                                        }
+                                    }
+                                } 
+                            })
+
+    
+
+    es.indices.put_template(name='errors_template',
+                            body={
+                                "template": "parse-errors*",
+                                "mappings": {
+                                    "parse-error": {
+                                        "properties": {
+                                            "log_filename": {
+                                                "type": "string",
+                                                "index": "not_analyzed",
+                                            },
+                                            "parse_time": {
+                                                "type": "date",
+                                            },
+                                            "log_date": {
+                                                "type": "date",
+                                            },
+                                            "s3_key": {
+                                                "type": "string",
+                                                "index": "not_analyzed",
+                                            },
+                                            "reason": {
+                                                "type": "string",
+                                                "index": "not_analyzed",
+                                            },
+                                            "log_folder": {
+                                                "type": "string",
+                                                "index": "not_analyzed",
+                                            },
+                                            "line": {
+                                                "type": "string",
+                                                "index": "not_analyzed",
+                                            },
+                                            "exception_message": {
+                                                "type": "string",
+                                                "index": "not_analyzed",
+                                            },
+                                            "exception_type": {
+                                                "type": "string",
+                                                "index": "not_analyzed",
+                                            },
+                                        }
+                                    }
+                                } 
+                            })
+
+    # enable gzip compression
+    # https://github.com/elastic/elasticsearch-py/issues/252
+    connection = es.transport.get_connection()
+    connection.headers.update(urllib3.make_headers(accept_encoding=True))
+
+
     for fname in fs:
         fname = '%s/%s' % (root, fname)
         if fname.endswith('.gz'):
@@ -22,56 +121,6 @@ def run():
                 print 'already done'
                 continue
             print 'uploading %s' % fname
-            try:
-                event = next(iter(events(fname)))
-            except StopIteration:
-                print 'no events in this file!'
-                completed.add(fname)
-                continue
-
-
-            index = event['_index']
-            if not es.indices.exists(index):
-                # es.indices.delete(index)
-                es.indices.create(index=index,body={
-                    "mappings": {
-                        "log": {
-                            "properties": {
-                                "ip_num": {
-                                    "type": "ip"
-                                },
-                                "time_local": {
-                                    "type": "date",
-                                },
-                                "s3_key": {
-                                    "type": "string",
-                                    "index": "not_analyzed",
-                                },
-                                "host": {
-                                    "type": "string",
-                                    "index": "not_analyzed",
-                                },
-                                "request": {
-                                    "type": "string",
-                                    "index": "not_analyzed",
-                                },
-                                "http_referer": {
-                                    "type": "string",
-                                    "index": "not_analyzed",
-                                },
-                                "http_path": {
-                                    "type": "string",
-                                    "index": "not_analyzed",
-                                },
-                                "http_request": {
-                                    "type": "string",
-                                    "index": "not_analyzed",
-                                },
-                            }
-                        }
-                    }
-                })
-
             elasticsearch.helpers.bulk(es, events(fname), request_timeout=500)
             
             completed.add(fname)
