770e5b7f20c9289112688150084408bda9312bfc
Adrian Smith <smith.adriane@gmail.com>
2016-10-05 14:03:06 -0700

    Fixes for pylint.

--- src/dparselog.py
+++ src/dparselog.py
@@ -1,7 +1,5 @@
 import re
 import itertools
-from elasticsearch import Elasticsearch
-import elasticsearch.helpers
 from datetime import tzinfo, timedelta, datetime
 import gzip
 import os, os.path
@@ -29,7 +27,7 @@ class FixedOffset(tzinfo):
 def parse_date(dt_str):
     naive_date_str, offset_str = dt_str.split(' ')
     naive_dt = datetime.strptime(naive_date_str, '%d/%b/%Y:%H:%M:%S')
-    
+
     offset = int(offset_str[-4:-2])*60 + int(offset_str[-2:])
     if offset_str[0] == "-":
         offset = -offset
@@ -44,23 +42,33 @@ def convert_bytes_sent(s):
         return int(s)
 
 def generate_regex(format_string, var_regex, regexps):
-    format_vars  =  re.findall(var_regex, format_string)
+    format_vars = re.findall(var_regex, format_string)
 
-    var_matches  =  list(re.finditer(var_regex, format_string))
+    var_matches = list(re.finditer(var_regex, format_string))
 
-    var_match_positions  =  [(match.start(),match.end()) for match in var_matches]
+    var_match_positions = [(match.start(), match.end())
+                           for match in var_matches]
 
-    non_var_indexes  =  [0] + list(itertools.chain(*var_match_positions)) + [len(format_string)]
+    non_var_indexes = (
+        [0] +
+        list(itertools.chain(*var_match_positions)) +
+        [len(format_string)]
+    )
 
-    grouped_non_var_indexes = [ (non_var_indexes[i*2],non_var_indexes[i*2+1]) for i in range(len(non_var_indexes)/2)]
+    grouped_non_var_indexes = [(non_var_indexes[i*2], non_var_indexes[i*2+1])
+                               for i in range(len(non_var_indexes)/2)]
 
-    non_var_strings = [format_string[start:end] for start,end in grouped_non_var_indexes]
+    non_var_strings = [format_string[start:end]
+                       for start, end in grouped_non_var_indexes]
     escaped_non_var_strings = [re.escape(s) for s in non_var_strings]
 
-    named_regexps = ['(' + regexps[s] + ')' for i,s in enumerate(format_vars)]
-    full_regex_pieces = list(itertools.chain(*itertools.izip_longest(escaped_non_var_strings, named_regexps,fillvalue='')))
+    named_regexps = ['(' + regexps[s] + ')' for i, s in enumerate(format_vars)]
+    full_regex_pieces = list(
+        itertools.chain(*itertools.izip_longest(escaped_non_var_strings,
+                                                named_regexps, fillvalue=''))
+    )
 
-    full_regex  =  ''.join(full_regex_pieces[:])
+    full_regex = ''.join(full_regex_pieces[:])
 
     return re.compile(full_regex)
 
@@ -71,8 +79,8 @@ class NginxLogParser(object):
         format_string = '$remote_addr - $remote_user [$time_local] "$request" $status $body_bytes_sent "$http_referer" "$http_user_agent" "$http_x_forwarded_for"\n'
 
         var_regex = r'\$[a-z_]+'
-        ip_num_regex  =  r'[0-9]{1,3}'
-        regexps  =  {
+        ip_num_regex = r'[0-9]{1,3}'
+        regexps = {
             '$ip_num'               : r'[0-9]{1,3}',
             '$remote_addr'          : '\\.'.join([ip_num_regex] * 4),
             '$remote_user'          : r'-',
@@ -84,18 +92,18 @@ class NginxLogParser(object):
             '$http_user_agent'      : r'[^"]*',
             '$http_x_forwarded_for' : r'[^"]+',
         }
-        self.format_vars  =  re.findall(var_regex, format_string)
+        self.format_vars = re.findall(var_regex, format_string)
         self.regex = generate_regex(format_string, var_regex, regexps)
         
 
     def parse(self, to_parse):
-        match  =  self.regex.match(to_parse)
+        match = self.regex.match(to_parse)
         if match:
             parsed = dict(zip(self.format_vars, match.groups()))
         else:
             return None
 
-        parsed = { k[1:]: v for k,v in parsed.items() }
+        parsed = {k[1:]: v for k, v in parsed.items() }
 
         field_types = {
             'status' : int,
@@ -124,10 +132,10 @@ class GunicornLogParser(object):
         format_string = '''%({X-Forwarded-For}i)s %(l)s %(u)s %(t)s "%(r)s" %(s)s %(b)s "%(f)s" "%(a)s"\n'''
 
         var_regex = r'%\([^)]+\)s'
-        ip_num_regex  =  r'[0-9]{1,3}'
+        ip_num_regex = r'[0-9]{1,3}'
 
         one_ip_regex = '\\.'.join([ip_num_regex] * 4)
-        two_ip_regex = r'%s, %s' % ( one_ip_regex, one_ip_regex)
+        two_ip_regex = r'%s, %s' % (one_ip_regex, one_ip_regex)
         x_forwarded_for_regex = r'(?:%s|%s)' % (one_ip_regex, two_ip_regex)
         regexps = {
             '%({X-Forwarded-For}i)s': x_forwarded_for_regex,
@@ -144,10 +152,10 @@ class GunicornLogParser(object):
 
         self.format_vars = re.findall(var_regex, format_string)
         self.regex = generate_regex(format_string, var_regex, regexps)
-        
+
 
     def parse(self, to_parse):
-        match  =  self.regex.match(to_parse)
+        match = self.regex.match(to_parse)
         if match:
             parsed = dict(zip(self.format_vars, match.groups()))
         else:
@@ -164,7 +172,7 @@ class GunicornLogParser(object):
             '%(f)s': 'http_referer',
             '%(a)s': 'http_user_agent',
         }
-        parsed = { better_names[k] : v for k,v in parsed.items()}
+        parsed = {better_names[k] : v for k, v in parsed.items()}
 
         request_regex = r'(?P<http_method>[A-Z]+) (?P<http_path>.*) HTTP/1.[01]'
         request_match = re.match(request_regex, parsed['request'])
@@ -186,8 +194,8 @@ class GunicornLogParser(object):
         return parsed
 
 
-gunicorn_test_string = '''108.252.136.229 - - [09/Aug/2016:10:15:32 -0700] "GET / HTTP/1.0" 500 3840 "-" "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.103 Safari/537.36"\n'''
-nginx_test_string = '''183.129.160.229 - - [20/Aug/2016:03:34:59 +0000] "GET / HTTP/1.1" 444 0 "-" "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.11; rv:47.0) Gecko/20100101 Firefox/47.0" "-"\n'''
+# gunicorn_test_string = '''108.252.136.229 - - [09/Aug/2016:10:15:32 -0700] "GET / HTTP/1.0" 500 3840 "-" "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.103 Safari/537.36"\n'''
+# nginx_test_string = '''183.129.160.229 - - [20/Aug/2016:03:34:59 +0000] "GET / HTTP/1.1" 444 0 "-" "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.11; rv:47.0) Gecko/20100101 Firefox/47.0" "-"\n'''
 
 def error_event(fname, key, reason, extra=None):
     now = datetime.now()
@@ -223,7 +231,7 @@ def generate_events(stream, key):
             log_type = 'djsession'
         else:
             log_type = 'customer'
-        
+
     else:
         log_type = None
 
@@ -249,8 +257,8 @@ def generate_events(stream, key):
 
     error_count = 0
     ok_count = 0
-    for i,line in stream:
-        line = line.decode('ascii',errors='replace')
+    for i, line in stream:
+        line = line.decode('ascii', errors='replace')
 
         total_count = ok_count + error_count
         if total_count > 100 and (float(error_count)/total_count) > 0.8:
@@ -282,7 +290,7 @@ def generate_events(stream, key):
         else:
             ok_count += 1
 
-        _id = '%s:%d' % (key,i)
+        _id = '%s:%d' % (key, i)
 
         event.update({
             's3_key' : key,
@@ -302,7 +310,7 @@ def generate_events(stream, key):
         }
 
 def sanitize_filename(fname):
-    fname  = fname.replace(os.path.sep, '_')
+    fname = fname.replace(os.path.sep, '_')
     fname = re.sub(r'[^a-zA-Z_\-.0-9]', '', fname)
     fname = re.sub(r'^[^a-zA-Z0-9]+', '', fname)
     if fname.startswith('.'):
@@ -311,7 +319,7 @@ def sanitize_filename(fname):
     return fname
 
 
-if __name__ == '__main__':
+def main():
     import sys
     root = sys.argv[1]
     key = sys.argv[2]
@@ -325,8 +333,8 @@ if __name__ == '__main__':
     serializer = JSONSerializer()
 
     try:
-        with gzip.open(outname,'wb') as out:
-            with open(os.path.join(root,key),mode='rb') as f:
+        with gzip.open(outname, 'wb') as out:
+            with open(os.path.join(root, key), mode='rb') as f:
                 gzfile = gzip.GzipFile(fileobj=f, mode='rb')
                 for event in generate_events(enumerate(gzfile), key):
                     # the elasticsearch serializer does have a
@@ -347,3 +355,5 @@ if __name__ == '__main__':
 
 
 
+if __name__ == '__main__':
+    main()
