c34617400561dda52caa6bc1ac4c0e4d3f036994
Morgan Shorter <morgan@morganshorter.com>
2021-03-30 16:06:11 -0700

    Fixed another typo

--- src/tero/setup/syslog.py
+++ src/tero/setup/syslog.py
@@ -140,7 +140,6 @@ log { source(s_sys); filter(f_5xxERR-hook); destination(d_5xxERR-hook); };
                 syslog_te_file.write(te_template.template)
             setup.postinst.install_selinux_module(syslog_te,
                 te_template.comment)
-            setup.postinst.shellCommand(['systemctl', 'reload', 'syslog-ng']
-
+            setup.postinst.shellCommand(['systemctl', 'reload', 'syslog-ng'])
 
         return complete
