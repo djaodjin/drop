fc87c2f58c36755347a6e5f829b048af296129fe
Sebastien Mirolo <smirolo@djaodjin.com>
2026-04-15 10:00:12 -0700

    upgrades prerequisites

--- src/tero/__init__.py
+++ src/tero/__init__.py
@@ -48,7 +48,7 @@ from __future__ import unicode_literals
 #
 # Requires Python 2.7 or above.
 
-__version__ = '1.1.0'
+__version__ = '1.1.1-dev'
 
 import datetime, getpass, hashlib, inspect, json, locale
 import logging, logging.config
